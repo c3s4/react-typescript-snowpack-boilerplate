0863a0f22039f35da4cbbf3c802b7128026a027a
Davide Cesaroni <davide.cesaroni@gmail.com>
2021-03-07 18:55:31 +0100

    Fix to be eslint compliant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-interface AppProps {}
-
-function App({}: AppProps) {
+const App = () => {
   // Create the count state.
   const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
@@ -24,18 +22,13 @@ function App({}: AppProps) {
           Page has been open for <code>{count}</code> seconds.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
